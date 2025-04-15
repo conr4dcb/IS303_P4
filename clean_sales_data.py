d11a8df06d14758aabd68208b49aac0dd7900973
Elise-Chapman <131200483+Elise-Chapman@users.noreply.github.com>
2025-04-14 19:30:24 -0600

    Created the Menu
    I am working on the importing the file still but I am having issues with my computer so I commented it out for a second

--- clean_sales_data.py
+++ clean_sales_data.py
@@ -4,6 +4,7 @@
 # This program will extract, transform, and load sales data into a postgres database using python.
 # From postgres, data will be then be fetched, analyzed, and vizualized using python.
 
+# import libraries
 import pandas as pd
 import numpy as np
 import sqlalchemy
@@ -11,3 +12,9 @@ from sqlalchemy import text
 import matplotlib.pyplot as plot
 import openpyxl
 import psycopg2 as pg2
+
+# import the excel file
+# pd.read_excel (r"C:\Users\Elise\Desktop\IS303\IS303_P4.xlsx")
+
+# Create the menu
+input("Menu: \nIf you want to import data, enter 1. \nIf you want to see summaries of stored data, enter 2. \nEnter any other value to exit the program: ")
