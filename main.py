de39147eb728c2d8b568356f8c5ab15fa2833581
Elise-Chapman <131200483+Elise-Chapman@users.noreply.github.com>
2025-04-14 18:41:52 -0600

    imported libraries

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-# Authors: Conrad Bradford, Blake Rogers, Haley Sommer
+# Authors: Conrad Bradford, Blake Rogers, Haley Sommer, Elise Chapman
 # IS 303 Section 003
-# # P4 - Retail Sales: Pandas and PostgreSQL
+# P4 - Retail Sales: Pandas and PostgreSQL
+
+import sqlalchemy
+import pandas as pd
+import matplotlib.pyplot as plot
+import openpyxl
+import psycopg2
+
+input ("Menu: \nIf you want to import data, enter 1. If you want to see summaries of stored data, enter 2. Enter any other value to exit the program: ")
+
